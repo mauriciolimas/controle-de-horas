216c1382b6e793b7727eeda5c8a0d5e8a0eb5640
mauriciolimas <maulima05@gmail.com>
2019-10-30 23:17:56 -0300

    Revert "Layout"

--- app/src/main/java/com/uninove/controledehoras/MainActivity.java
+++ app/src/main/java/com/uninove/controledehoras/MainActivity.java
@@ -1,12 +1,10 @@
 package com.uninove.controledehoras;
 
-import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
-import android.util.Log;
 import android.view.View;
 
 import androidx.navigation.NavController;
@@ -22,29 +20,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.Menu;
-import android.widget.Button;
-import android.widget.TextView;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
-public class MainActivity extends AppCompatActivity implements ListHour.OnFragmentInteractionListener  {
+public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
-
-    private Button btnAction;
-    private TextView startHour;
-    private TextView startInterval;
-    private TextView endInterval;
-    private TextView endHour;
-    private TextView totalHour;
-
-    private List list;
 
 
     @Override
@@ -60,19 +39,12 @@ public class MainActivity extends AppCompatActivity implements ListHour.OnFragme
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow,
-                R.id.nav_tools, R.id.nav_share, R.id.nav_send, R.id.listHour)
+                R.id.nav_tools, R.id.nav_share, R.id.nav_send)
                 .setDrawerLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-
-
-        this.btnAction = findViewById(R.id.btn_action);
-
-        list = new ArrayList<LocalDateTime>();
-
-
     }
 
     @Override
@@ -89,75 +61,4 @@ public class MainActivity extends AppCompatActivity implements ListHour.OnFragme
                 || super.onSupportNavigateUp();
     }
 
-    public String formatHour(Long ms){
-        int segundos = (int) Math.round( ( ms / 1000 ) % 60);
-        int minutos  = (int) Math.round( ( ms / 60000 ) % 60);
-        int horas    = (int) Math.round ( (ms / 3600000) );
-        return String.format( "%02d:%02d:%02d", horas, minutos,segundos );
-    }
-
-    public Long differenceHours(LocalDateTime t1, LocalDateTime t2) {
-        return t1.until(t2, ChronoUnit.MILLIS);
-    }
-
-
-    public void getHour(View v) {
-
-
-
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
-        LocalDateTime localDate = LocalDateTime.now();
-
-        this.list.add(localDate);
-
-        startHour = findViewById(R.id.hour_start);
-        startInterval = findViewById(R.id.start_interval);
-        endInterval = findViewById(R.id.end_interval);
-        endHour = findViewById(R.id.hour_end);
-        totalHour = findViewById(R.id.total_hour);
-
-
-
-        switch (list.size()){
-            case 1:
-                startHour.setText(( (LocalDateTime) list.get(0) ).format(formatter));
-                break;
-            case 2:
-                startInterval.setText( ((LocalDateTime) list.get(1)).format((formatter)) );
-
-                LocalDateTime t1 = (LocalDateTime) list.get(0);
-                LocalDateTime t2 = (LocalDateTime) list.get(1);
-
-                totalHour.setText(this.formatHour(this.differenceHours(t1, t2)));
-
-                break;
-            case 3:
-                endInterval.setText(( (LocalDateTime) list.get(2) ).format(formatter));
-                break;
-            case 4:
-                endHour.setText( ((LocalDateTime) list.get(3)).format((formatter)) );
-
-                Long int1 = this.differenceHours((LocalDateTime) list.get(0), (LocalDateTime) list.get(1));
-                Long int2 = this.differenceHours((LocalDateTime) list.get(2), (LocalDateTime) list.get(3));
-
-                totalHour.setText( this.formatHour( int1 + int2 ) );
-
-                break;
-            default:
-                startHour.setText("00:00:00");
-                startInterval.setText("00:00:00");
-                endInterval.setText("00:00:00");
-                endHour.setText("00:00:00");
-                totalHour.setText("00:00:00");
-                list.removeAll(list);
-                break;
-
-        }
-
-    }
-
-    @Override
-    public void onFragmentInteraction(Uri uri) {
-
-    }
 }
